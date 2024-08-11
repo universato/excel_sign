b711cee2e9b541f23ee56dac75f7a0ed76bcb373
universato <universato@gmail.com>
2024-08-11 17:59:57 +0900

    sign_excel_winauto.py

--- sign_excel_winauto.py
+++ sign_excel_winauto.py
@@ -3,7 +3,8 @@ import re
 import sys
 from pywinauto import Application
 from pywinauto.keyboard import send_keys
-from pyautogui import screenshot
+from PIL import ImageGrab
+# from pyautogui import screenshot
 from time import sleep
 
 # pip install pywinauto
@@ -11,33 +12,41 @@ from time import sleep
 
 EXCEL_APP_PATH = r"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"
 
-def main():
+
+def main() -> None:
     # find_executabl_excel_app_paths()
     excel_file_path = r'C:\Users\unive\dev\vba_labo\a.xlsm'
     # excel_file_path = r'C:\Users\unive\dev\vba_labo\not_found.xlsm'
     # excel_file_path = r'C:\Users\unive\dev\vba_labo\a.png'
     # sign_excel(excel_file_path)
-    screenshot_excel_signature(excel_file_path, region=(1371,13,544,782))
+    excel_app = open_excel_and_open_signature_dialog(excel_file_path)
+    # print(excel_app[u"デジタル署名"].exists())
+    # screenshot_excel_signature(excel_file_path)
+    # screenshot_excel_signature(excel_file_path, region=(1371,283,542,785))
+    # screenshot_excel_signature(excel_file_path, region=(1371, 13, 544, 782))
 
 
+def sign_excel(excel_file_path: str):
+    """
+    # 機能: 引数のExcelファイルを開いて、署名して、上書き保存して閉じる.
+    # 引数: 署名したいExcelファイルのパス文字列.
+    """
 
-# 機能: 引数のExcelファイルを開いて、署名して、上書き保存して閉じる.
-def sign_excel(excel_file_path):
     # Excelファイルを立ち上げ、｢デジタル署名｣ダイアログを開く.
     excel_app = open_excel_and_open_signature_dialog(excel_file_path)
     if excel_app is None:
         return
 
     # ｢Windowsセキュリティ｣を開く.
-    send_keys('%C') # [選択(C)] Alt + C
+    send_keys('C')  # [選択(C)]
     sleep(0.25)
 
     # ｢Windowsセキュリティ｣で[OK]で証明書を付与.
-    send_keys('{TAB} {ENTER}') # [証明書のプロパティを表示します] -> [OK]
+    send_keys('{TAB} {ENTER}')  # [証明書のプロパティを表示します] -> [OK]
     sleep(0.25)
 
     # ｢デジタル署名｣を[OK]で閉じる
-    send_keys('{TAB} {ENTER}') # [選択(C)] -> [OK]
+    send_keys('{TAB} {ENTER}')  # [選択(C)] -> [OK]
     sleep(0.25)
 
     # ｢Visual Basic｣を閉じる
@@ -45,14 +54,21 @@ def sign_excel(excel_file_path):
     sleep(0.25)
 
     # 上書き保存
-    send_keys('% F S') # Alt -> F -> S
+    send_keys('% F S')  # Alt -> F -> S
     sleep(4.75)
 
     excel_app.kill()
 
-# 機能: 引数のExcelファイルを立ち上げ、｢デジタル署名｣ダイアログまで開く.
-# 返り値: 立ち上げたExcelファイル
-def open_excel_and_open_signature_dialog(excel_file_path):
+    return True
+
+
+def open_excel_and_open_signature_dialog(excel_file_path: str):
+    """
+    # 機能: 引数のExcelファイルを立ち上げ、｢デジタル署名｣ダイアログまで開く.
+    # 引数: ｢デジタル署名｣ダイアログを開きたい、Excelファイルのパス文字列。
+    # 返り値: 立ち上げたExcelファイル. 開くことが出来なければ、Noneを返す。
+    """
+
     # 引数のExcelファイルが存在しなければ、Noneを返し、呼び出し元に処理を中止してもらう.
     if not os.path.exists(excel_file_path):
         print(f"[ALERT] Excel file Not Found: {excel_file_path}")
@@ -74,24 +90,30 @@ def open_excel_and_open_signature_dialog(excel_file_path):
     # 探しても、見つからなければエラー終了。
     find_excel_exe()
 
-    # Excelを開く
+    # Excelファイルを開く
     excel_app = Application().start(f'"{EXCEL_APP_PATH}" "{excel_file_path}"')
     sleep(4.75)
 
     # ｢Visual Basic｣を開く.
-    send_keys('% L V') # 順番に、Alt -> L -> V
+    send_keys('% L V')  # 順番に、Alt -> L -> V
     sleep(0.75)
 
     # ｢デジタル署名｣を開く.
-    send_keys('%TD') # Altを押したまま、T -> D. Excel16以降は、Altを1度離した(Alt + T) -> (Alt + D) も可.
+    # Altを押したまま、T -> D. Excel16以降は、Altを1度離した(Alt + T) -> (Alt + D) も可.
+    send_keys('%TD') # Alt -> T -> D
     sleep(0.75)
 
     return excel_app
 
 
-# 機能: Exccelに署名して、保存する.
-# 前提: 署名対象のExcelフィルの｢デジタル署名｣ダイアログが開いてフォーカスがあたっている状態で使う.
-def screenshot_excel_signature(excel_file_path, region=None, output_folder=None):
+def screenshot_excel_signature(excel_file_path: str, region=None, output_folder=None):
+    """
+    # 機能: Exccelファイルに署名して、保存する.
+    # 引数: excel_file_path: 署名して保存したいExcelファイルのパス文字列.
+    # 引数: region: スクリーンショットする座標･サイズ. (left, top, width, height)で表現する. Noneなら全画面.
+    # 引数: output_folder: スクリーンショットを保存するフォルダ.
+    """
+
     # 引数output_folderに指定がなければ、スクリーンショットの保存先を対象ファイルと同じフォルダにする.
     if output_folder is None:
         output_folder = os.path.split(excel_file_path)[0]
@@ -102,39 +124,49 @@ def screenshot_excel_signature(excel_file_path, region=None, output_folder=None)
         return
 
     # [詳細]から、｢証明書｣画面を開く.
-    send_keys('%D')  # [詳細(D)] Alt + D
+    send_keys('D')  # [詳細(D)]
     sleep(0.50)
 
-    excel_filename = os.path.basename(excel_file_path) # 拡張子あり
-    # excel_filename = os.path.splitext(os.path.basename(excel_file_path))[0] # 拡張子なし
+    excel_filename = os.path.basename(excel_file_path)  # 拡張子あり
+    # excel_filename = os.path.splitext(os.path.basename(excel_file_path))[0]  # 拡張子なし
 
-    send_keys('{TAB}') # [全般タブ]
+    send_keys('{TAB}')  # [全般タブ]
     screenshot(f"{output_folder}\\{excel_filename}_1_全般.png", region=region)
     sleep(0.25)
 
-    send_keys('{RIGHT}') # [全般]タブ -> [詳細]タブ
+    send_keys('{RIGHT}')  # [全般]タブ -> [詳細]タブ
     screenshot(f"{output_folder}\\{excel_filename}_2_詳細.png", region=region)
     sleep(0.25)
 
-    send_keys('{RIGHT}') # [詳細]タブ -> [証明書のパス]タブ
+    send_keys('{RIGHT}')  # [詳細]タブ -> [証明書のパス]タブ
     screenshot(f"{output_folder}\\{excel_filename}_3_パス.png", region=region)
-    sleep(0.25)
-
-    # ｢証明書｣画面を閉じ、｢デジタル署名｣ダイアログを閉じる.
-    send_keys('{ESC} {ESC}')
     sleep(0.25)
 
     excel_app.kill()
 
-# 機能: EXCEL.EXEが立ち上がっているかどうか真偽値を返す.
+
 def is_excel_running() -> bool:
+    """
+    # 機能: Excelが立ち上がっているかどうか真偽値を返す.
+    # 返り値: Excelファイルが開いていることがわかれば、True. 開いてなさそうなら、False.
+    # その他: from pywinauto import Application
+    """
     try:
         excel_app = Application().connect(path="EXCEL.EXE", timeout=1)
         return True
     except:
         return False
 
-def find_executabl_excel_app_paths(message=True):
+
+def find_executabl_excel_app_paths(message: bool) -> list[str]:
+    """
+    # 機能: 実行可能なEXCEL.EXEのあるパスを返す.
+    # 詳細: 12通り(最大24通り)の可能性の高いパスにEXCEL.EXEがあるか探して、存在するパスのリストを返す.
+    # 引数: message: Trueなら見つかったパスを出力する.
+    # 返り値: 見つかったEXCEL.EXEのパス文字列のリスト
+    # その他: import os
+    # その他: C:/Program Files (x86)/Microsoft Office/root/Office16/EXCEL.EXEのようなパスを探す.
+    """
     program_paths = [
         r"C:\Program Files",
         r"C:\Program Files (x86)",
@@ -147,7 +179,8 @@ def find_executabl_excel_app_paths(message=True):
     for program_path in program_paths:
         for root in ["", "root"]:
             for version in ["Office14", "Office15", "Office16"]:
-                possible_paths.append(os.path.join(program_path, "Microsoft Office", root, version, "EXCEL.EXE"))
+                possible_paths.append(os.path.join(
+                    program_path, "Microsoft Office", root, version, "EXCEL.EXE"))
     # print("\n".join(possible_paths))
 
     exist_paths = []
@@ -164,13 +197,14 @@ def find_executabl_excel_app_paths(message=True):
 
     return exist_paths
 
-# 機能: EXCEL.EXEを探す
-# 詳細: EXCEL.EXEが探しても見つからなかった場合、エラーを返して、sys.exit(1)でプログラム全体を終了する。
-#       そのため、この関数が2回目の場合は、既にEXCEL.EXEを見つけているはずなので、何度も
-#       EXCEL.EXEが見つかった場合、excel_app_existsをTrueにし、何度も探さないようにする。
-#       変数EXCEL_APP_PATHにあるPATHに、EXCEL.EXEがあれば、それを使う。
-#       ここになければ、可能性のあるPATHを列挙して、
+
 def find_excel_exe() -> None:
+    """
+    # 機能: EXCEL.EXEを探す
+    # 詳細: 変数EXCEL_APP_PATHにあるPATHに、EXCEL.EXEがあれば、それで良い.
+    #       存在しなければ存在する可能性の高いパスを探して、EXCEL.EXE見つけ出してEXCEL_APP_PATHを更新.
+    #       EXCEL.EXEが見つからなかった場合、エラーを返して、sys.exit(1)でプログラム全体を終了.
+    """
     global EXCEL_APP_PATH
 
     if os.path.exists(EXCEL_APP_PATH):
@@ -188,6 +222,17 @@ def find_excel_exe() -> None:
         print(f"\t入っているなら、EXCEL_APP_PATHにEXCEL.EXEのパスを代入して下さい。")
         sys.exit(1)
 
-main()
+def screenshot(output_file_path, region=None):
+    bbox = None
+    if region:
+        left,top,width,height = region
+        right  = left + width
+        bottom = top  + height
+        bbox = (left,top,right,bottom)
+    screenshot = ImageGrab.grab(bbox=bbox)
+    screenshot.save(output_file_path)
+
+if __name__=="__main__":
+    main()
 # pip install pywinauto
 # pip install pyautogui pyscreeze Pillow
